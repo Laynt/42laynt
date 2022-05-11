ff6ed56f578acd7facef12c1a60614a5a355990b
Laynt01 <alvaro.to.cia@gmail.com>
2022-05-11 21:39:28 +0200

    no se que pasa

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: alvtorre <alvtorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 21:54:01 by alvtorre          #+#    #+#             */
-/*   Updated: 2022/05/11 19:33:54 by alvtorre         ###   ########.fr       */
+/*   Updated: 2022/05/11 21:31:49 by alvtorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
-int		*ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*strnstr(const char *haystack, const char *needle, size_t len);
 #endif
